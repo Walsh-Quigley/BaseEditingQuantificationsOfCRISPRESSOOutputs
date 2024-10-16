0cdd88686cb9c4c24e83f51c72a2890d973356c6
Walsh Quigley <86450744+Walsh-Quigley@users.noreply.github.com>
2024-10-16 13:54:27 -0700

    to be completed

---
+++ userIn.sh
@@ -0,0 +1,13 @@
+
+
+for line in amp_list:
+    #hello user
+    #we are on line 1
+    #please enter intended and permissble subs for line 1
+    input("intended or permissible? (i/p)")
+        #user hits i
+        if userIn == i:
+
+        if userIn == p:
+
+    #in the form of 3,4,5,6,7
